74bcf4a3c067d8630f57adb1852a6a7ab17ae8d9
Gaash.TalmiGranot <Gaash.TalmiGranot@sizmek.com>
2018-08-08 10:20:26 +0300

    initial message

---
+++ src/main/java/main/AnotherFile.java
@@ -0,0 +1,5 @@
+package main;
+
+public class AnotherFile {
+    private String field;
+}
---
+++ src/main/java/main/ProjectMainClass.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ProjectMainClass {
+    public static void main(String[] args) {
+        ProjectMainClass main = new ProjectMainClass();
+        main.printWelcomeMessage();
+    }
+
+    private void printWelcomeMessage() {
+        System.out.println("Hello git world!!!!");
+    }
+}
